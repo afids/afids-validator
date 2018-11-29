1438b0450e8f0181f0b4501278000abafd26e48a
Park-Patrick <parkpatrickj@gmail.com>
2018-11-29 01:08:50 -0500

    add auto page and extension validation

--- home/fidvalidator/controller.py
+++ home/fidvalidator/controller.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template, request
 from compute import calc
 from model import InputForm
+import os
+from compute_auto import compute_mean_std as compute_function
+from model_auto import Average
 
 app = Flask(__name__)
 
-@app.route('/vib1', methods=['GET', 'POST'])
+# Relative path of directory for uploaded files
+UPLOAD_DIR = 'uploads/'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_DIR
+app.secret_key = 'MySecretKey'
+
+if not os.path.isdir(UPLOAD_DIR):
+    os.mkdir(UPLOAD_DIR)
+
+# Allowed file types for file upload
+ALLOWED_EXTENSIONS = set(['txt', 'fcsv', 'csv'])
+
+def allowed_file(filename):
+    """Does filename have the right extension?"""
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/home', methods=['GET', 'POST'])
 def index():
 	form = InputForm(request.form)
 	if request.method == 'POST' and form.validate():
@@ -110,6 +130,28 @@ def index():
 		L_olf_sulc_result=L_olf_sulc_result
 		# need to complete this list
 		)
+
+# Path to the web application
+@app.route('/auto', methods=['GET', 'POST'])
+def index2():
+    form = Average(request.form)
+    filename = None  # default
+    if request.method == 'POST':
+
+        # Save uploaded file on server if it exists and is valid
+        if request.files:
+            file = request.files[form.filename.name]
+            if file and allowed_file(file.filename):
+                # Make a valid version of filename for any file ystem
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'],
+                                       filename))
+
+        result = compute_function(filename)
+    else:
+        result = None
+
+    return render_template("view.html", form=form, result=result)
 
 if __name__ == '__main__':
 	app.run(debug=True)
