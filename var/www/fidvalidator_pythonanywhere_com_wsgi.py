ed24fcb458544587c77e8a274bcb5710372e185b
kaitj <tkai@uwo.ca>
2018-12-05 14:11:10 -0500

    changes for local development
    - added .gitignore
    - changed path to point to correct <dir>/home/fidvalidator directory in wsgi.py

--- var/www/fidvalidator_pythonanywhere_com_wsgi.py
+++ var/www/fidvalidator_pythonanywhere_com_wsgi.py
@@ -1,7 +1,9 @@
 import sys
-path = '/home/fidvalidator/'
+# path = '/home/fidvalidator' 
+path = '/home/tkai/git/fidvalidator/home/fidvalidator'
 
 if path not in sys.path:
     sys.path.append(path)
 
+print("TEST")
 from controller import app as application
