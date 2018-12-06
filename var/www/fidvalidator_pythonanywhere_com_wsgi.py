117cc24e741b7b39a3b1be1489bde33046accae3
kaitj <tkai@uwo.ca>
2018-12-06 12:05:19 -0500

    correct path for wgsi.py

--- var/www/fidvalidator_pythonanywhere_com_wsgi.py
+++ var/www/fidvalidator_pythonanywhere_com_wsgi.py
@@ -1,9 +1,7 @@
 import sys
-# path = '/home/fidvalidator' 
-path = '/home/tkai/git/fidvalidator/home/fidvalidator'
+path = '/home/fidvalidator'
 
 if path not in sys.path:
     sys.path.append(path)
 
-print("TEST")
 from controller import app as application
