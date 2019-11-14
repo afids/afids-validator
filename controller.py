b35b7ef86dcfe885aba4ae9a6af9b1edcd7755cc
Tristan Kuehn <tristankuehn@gmail.com>
2019-11-14 12:28:15 -0500

    Move db definition

--- controller.py
+++ controller.py
@@ -28,41 +28,6 @@ if not os.path.isdir(UPLOAD_DIR):
 
 # Allowed file types for file upload
 ALLOWED_EXTENSIONS = set(['fcsv', 'csv'])
-
-class Fiducial_set(db.Model):
-    __tablename__ = 'fid_db'
-
-    id = db.Column(db.Integer, primary_key=True)
-    AC_x = db.Column(db.Float())
-    AC_y = db.Column(db.Float())
-    AC_z = db.Column(db.Float())
-    PC_x = db.Column(db.Float())
-    PC_y = db.Column(db.Float())
-    PC_z = db.Column(db.Float())
-
-    def __init__(self, AC_x, AC_y, AC_z, PC_x, PC_y, PC_z):
-        self.AC_x = AC_x
-        self.AC_y = AC_y
-        self.AC_z = AC_z
-        self.PC_x = PC_x
-        self.PC_y = PC_y
-        self.PC_z = PC_z
-
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
-
-    def serialize(self):
-        return {
-            'id': self.id, 
-            'AC_x': self.AC_x,
-            'AC_y': self.AC_y,
-            'AC_z': self.AC_z,
-            'PC_x': self.PC_x,
-            'PC_y': self.PC_y,
-            'PC_z': self.PC_z,
-
-        }
-
 def allowed_file(filename):
     """Does filename have the right extension?"""
     return '.' in filename and \
