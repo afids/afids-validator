2383710e753b8906b6bd113666cc049cdb801781
Tristan Kuehn <tristankuehn@gmail.com>
2019-11-14 12:16:04 -0500

    Adjust manage

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate, MigrateCommand
 from controller import app, db
 
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 migrate = Migrate(app, db)
 manager = Manager(app)
