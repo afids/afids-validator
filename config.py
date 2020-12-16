1db7e5a1ce2a063f2daa787fa37001391b45dbde
Tristan Kuehn <tristankuehn@gmail.com>
2020-12-16 16:40:32 -0500

    Lint all the python code

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
+"""Configuration classes for flask/heroku."""
+
 import os
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config(object):
+
+class Config():
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SECRET_KEY = "this-really-needs-to-be-changed"
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
 
 
 class ProductionConfig(Config):
--- visualizations.py
+++ visualizations.py
@@ -22,12 +22,19 @@ def gen_connecting_lines(ref_json, user_json):
     connecting_lines = []
     for ref_entry, user_entry in zip(ref_json.values(), user_json.values()):
         connecting_lines.append(
-            [{'x': float(ref_entry['x']),
-              'y': float(ref_entry['y']),
-              'z': float(ref_entry['z'])},
-             {'x': float(user_entry['x']),
-              'y': float(user_entry['y']),
-              'z': float(user_entry['z'])}])
+            [
+                {
+                    "x": float(ref_entry["x"]),
+                    "y": float(ref_entry["y"]),
+                    "z": float(ref_entry["z"]),
+                },
+                {
+                    "x": float(user_entry["x"]),
+                    "y": float(user_entry["y"]),
+                    "z": float(user_entry["z"]),
+                },
+            ]
+        )
     return connecting_lines
 
 
@@ -54,24 +61,32 @@ def calculate_magnitudes(connecting_lines):
     lines_z = []
     lines_magnitudes = []
     for line in connecting_lines:
-        lines_x += [line[0]["x"],
-                    (line[0]["x"] + line[1]["x"])/2,
-                    line[1]["x"],
-                    None]
+        lines_x += [
+            line[0]["x"],
+            (line[0]["x"] + line[1]["x"]) / 2,
+            line[1]["x"],
+            None,
+        ]
 
-        lines_y += [line[0]['y'],
-                    (line[0]['y'] + line[1]['y'])/2,
-                    line[1]['y'],
-                    None]
+        lines_y += [
+            line[0]["y"],
+            (line[0]["y"] + line[1]["y"]) / 2,
+            line[1]["y"],
+            None,
+        ]
 
-        lines_z += [line[0]['z'],
-                    (line[0]['z'] + line[1]['z'])/2,
-                    line[1]['z'],
-                    None]
+        lines_z += [
+            line[0]["z"],
+            (line[0]["z"] + line[1]["z"]) / 2,
+            line[1]["z"],
+            None,
+        ]
 
-        distance = ((line[0]['x']-line[1]['x'])**2
-                    + (line[0]['y']-line[1]['y'])**2
-                    + (line[0]['z']-line[1]['z'])**2)**0.5
+        distance = (
+            (line[0]["x"] - line[1]["x"]) ** 2
+            + (line[0]["y"] - line[1]["y"]) ** 2
+            + (line[0]["z"] - line[1]["z"]) ** 2
+        ) ** 0.5
         lines_magnitudes += [distance, distance, distance, 0]
 
     return (lines_x, lines_y, lines_z, lines_magnitudes)
@@ -97,60 +112,69 @@ def generate_3d_scatter(ref_json, user_json):
     connecting_lines = gen_connecting_lines(ref_json, user_json)
 
     lines_x, lines_y, lines_z, lines_magnitudes = calculate_magnitudes(
-        connecting_lines)
+        connecting_lines
+    )
 
-    ids = [entry['desc'] for entry in ref_json.values()]
+    ids = [entry["desc"] for entry in ref_json.values()]
 
     dset1 = [
         go.Scatter3d(
-            x=[float(i['x']) for i in ref_json.values()],
-            y=[float(i['y']) for i in ref_json.values()],
-            z=[float(i['z']) for i in ref_json.values()],
+            x=[float(i["x"]) for i in ref_json.values()],
+            y=[float(i["y"]) for i in ref_json.values()],
+            z=[float(i["z"]) for i in ref_json.values()],
             showlegend=True,
             mode="markers",
             marker=dict(
                 size=4,
-                color='rgba(255,191,31,0.9)',
-                line=dict(width=1.5, color='rgba(50,50,50,1.0)')),
-            hovertemplate=("%{text}<br>x: %{x:.4f}<br>y: %{y:.4f}<br>"
-                           + "z: %{z:.4f}"),
-            text=['<b>{0}</b>'.format(ids[int(i)]) for i in range(len(ids))],
-            name="Template AFIDs"),
+                color="rgba(255,191,31,0.9)",
+                line=dict(width=1.5, color="rgba(50,50,50,1.0)"),
+            ),
+            hovertemplate=(
+                "%{text}<br>x: %{x:.4f}<br>y: %{y:.4f}<br>" + "z: %{z:.4f}"
+            ),
+            text=["<b>{0}</b>".format(ids[int(i)]) for i in range(len(ids))],
+            name="Template AFIDs",
+        ),
         go.Scatter3d(
-            x=[float(i['x']) for i in user_json.values()],
-            y=[float(i['y']) for i in user_json.values()],
-            z=[float(i['z']) for i in user_json.values()],
+            x=[float(i["x"]) for i in user_json.values()],
+            y=[float(i["y"]) for i in user_json.values()],
+            z=[float(i["z"]) for i in user_json.values()],
             showlegend=True,
             mode="markers",
             marker=dict(
                 size=4,
-                color='rgba(0,0,0,0.9)',
-                line=dict(width=1.5, color='rgba(50,50,50,1.0)')),
-            hovertemplate=("%{text}<br>x: %{x:.4f}<br>y: %{y:.4f}<br>"
-                           + "z: %{z:.4f}"),
-            text=['<b>{0}</b>'.format(ids[int(i)]) for i in range(len(ids))],
-            name="Uploaded AFIDs"),
+                color="rgba(0,0,0,0.9)",
+                line=dict(width=1.5, color="rgba(50,50,50,1.0)"),
+            ),
+            hovertemplate=(
+                "%{text}<br>x: %{x:.4f}<br>y: %{y:.4f}<br>" + "z: %{z:.4f}"
+            ),
+            text=["<b>{0}</b>".format(ids[int(i)]) for i in range(len(ids))],
+            name="Uploaded AFIDs",
+        ),
         go.Scatter3d(
             x=lines_x,
             y=lines_y,
             z=lines_z,
             showlegend=False,
             mode="lines",
-            hovertemplate='%{text}',
+            hovertemplate="%{text}",
             text=[
-                '<b>{0}</b><br>Euclidean Distance: {1:.3f} mm'.format(
-                    ids[int(i/4)], lines_magnitudes[i])
-                for i in range(len(lines_x))],
+                "<b>{0}</b><br>Euclidean Distance: {1:.3f} mm".format(
+                    ids[int(i / 4)], lines_magnitudes[i]
+                )
+                for i in range(len(lines_x))
+            ],
             line=dict(
                 color=lines_magnitudes,
                 colorscale="Bluered",
                 width=8,
                 showscale=True,
-                colorbar=dict(
-                    title=dict(
-                        text="Euclidean distance"))),
-            name="Euclidean Distance"
-            )]
+                colorbar=dict(title=dict(text="Euclidean distance")),
+            ),
+            name="Euclidean Distance",
+        ),
+    ]
 
     bigfig = go.Figure()
     bigfig.add_trace(dset1[0])
@@ -161,12 +185,11 @@ def generate_3d_scatter(ref_json, user_json):
         title_text="Euclidean distances from template",
         autosize=True,
         barmode="stack",
-        coloraxis=dict(colorscale='Bluered'),
-        legend_orientation="h")
+        coloraxis=dict(colorscale="Bluered"),
+        legend_orientation="h",
+    )
 
-    return bigfig.to_html(
-        include_plotlyjs="cdn",
-        full_html=False)
+    return bigfig.to_html(include_plotlyjs="cdn", full_html=False)
 
 
 def generate_histogram(ref_json, user_json):
@@ -186,37 +209,43 @@ def generate_histogram(ref_json, user_json):
         between pairs of provided AFIDs.
     """
     connecting_lines = gen_connecting_lines(ref_json, user_json)
-    _, _, _, lines_magnitudes = calculate_magnitudes(
-        connecting_lines)
+    _, _, _, lines_magnitudes = calculate_magnitudes(connecting_lines)
 
-    ids = [entry['desc'] for entry in ref_json.values()]
+    ids = [entry["desc"] for entry in ref_json.values()]
 
     # next figure: histogram of distances
     lines_magnitudes_unique = [
-        i for ix, i in enumerate(lines_magnitudes) if not ix % 4]
+        i for ix, i in enumerate(lines_magnitudes) if not ix % 4
+    ]
 
-    howtosort = sorted(range(len(lines_magnitudes_unique)),
-                       key=lambda k: lines_magnitudes_unique[k])
+    howtosort = sorted(
+        range(len(lines_magnitudes_unique)),
+        key=lambda k: lines_magnitudes_unique[k],
+    )
     dists_sorted = [lines_magnitudes_unique[i] for i in howtosort]
     ids_sorted = [ids[i] for i in howtosort]
 
-    fig4 = go.Figure(data=go.Bar(
-        x=do_binning(dists_sorted),
-        y=[1 for i in ids],
-        text=[str(i) + '<br>' + str(round(dists_sorted[ix], 3)) + ' mm'
-              for ix, i in enumerate(ids_sorted)],
-        marker_color=dists_sorted,
-        marker_colorscale="Bluered",
-        showlegend=False))
+    fig4 = go.Figure(
+        data=go.Bar(
+            x=do_binning(dists_sorted),
+            y=[1 for i in ids],
+            text=[
+                str(i) + "<br>" + str(round(dists_sorted[ix], 3)) + " mm"
+                for ix, i in enumerate(ids_sorted)
+            ],
+            marker_color=dists_sorted,
+            marker_colorscale="Bluered",
+            showlegend=False,
+        )
+    )
 
     fig4.update_layout(
         title_text="Template vs. provided AFIDs",
         autosize=True,
-        coloraxis=dict(colorscale='Bluered'))
+        coloraxis=dict(colorscale="Bluered"),
+    )
 
-    return fig4.to_html(
-        include_plotlyjs="cdn",
-        full_html=False)
+    return fig4.to_html(include_plotlyjs="cdn", full_html=False)
 
 
 def do_binning(in_data, nbins=6):
