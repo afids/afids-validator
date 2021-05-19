398c206c129d3321ee331f53ea14103ad9bd4899
Lint Action <lint-action@samuelmeuli.com>
2021-05-19 02:04:34 +0000

    Fix code style issues with Black

--- afids.py
+++ afids.py
@@ -42,7 +42,7 @@ EXPECTED_MAP = dict(zip(EXPECTED_LABELS, EXPECTED_DESCS))
 
 
 def tree():
-    """ Create a tree structure """
+    """Create a tree structure"""
     return defaultdict(tree)
 
 
@@ -50,13 +50,13 @@ class Afids:
     """Class representing a complete set of AFIDs."""
 
     def __init__(self, coordinate_system="LPS", no_of_fiducials=0):
-        """ Initialize tree structure to store fiducial point info """
+        """Initialize tree structure to store fiducial point info"""
         self.fiducials = tree()
         self.coordinate_system = coordinate_system
         self.no_of_fiducials = no_of_fiducials
 
     def add_fiducial(self, label, description, positions):
-        """ Add fiducial to tree """
+        """Add fiducial to tree"""
         self.fiducials[str(label)]["description"] = description
         self.set_fiducial_positions(label, positions)
 
@@ -64,21 +64,21 @@ class Afids:
         self.no_of_fiducials += 1
 
     def get_fiducial_description(self, label):
-        """ Extract fiducial description """
+        """Extract fiducial description"""
         return self.fiducials[str(label)]["description"]
 
     def set_fiducial_positions(self, label, positions):
-        """ Set position of fiducial """
+        """Set position of fiducial"""
         self.fiducials[str(label)]["positions"]["x"] = positions[0]
         self.fiducials[str(label)]["positions"]["y"] = positions[1]
         self.fiducials[str(label)]["positions"]["z"] = positions[2]
 
     def set_fiducial_position(self, label, dim, position):
-        """ Set position for single dimension of fiducial """
+        """Set position for single dimension of fiducial"""
         self.fiducials[str(label)]["positions"][dim] = position
 
     def get_fiducial_positions(self, label):
-        """ Get fiducial position """
+        """Get fiducial position"""
         positions = (
             self.fiducials[str(label)]["positions"]["x"],
             self.fiducials[str(label)]["positions"]["y"],
@@ -88,7 +88,7 @@ class Afids:
         return positions
 
     def get_fiducial_position(self, label, dim):
-        """ Get single dimension position of fiducial """
+        """Get single dimension position of fiducial"""
         return self.fiducials[str(label)]["positions"][dim]
 
     def validate(self):
