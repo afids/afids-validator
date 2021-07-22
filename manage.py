831f7c6edd8c46cf7fb87af238bb48bffc6e1b56
Jason Kai <tkai@uwo.ca>
2021-07-22 09:12:13 -0400

    lint manage.py

--- manage.py
+++ manage.py
@@ -10,6 +10,7 @@ from afidsvalidator.model import db
 
 app = create_app()
 
+<<<<<<< HEAD
 app.config.from_object(os.environ["APP_SETTINGS"])
 
 postgres_uri = os.environ["DATABASE_URL"]
@@ -17,6 +18,21 @@ if postgres_uri.startswith("postgres://"):
     postgres_uri.replace("postgres://", "postgresql://", 1)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
 
+=======
+if os.environ.get("FLASK_ENV").lower() == "production":
+    from config import ProductionConfig
+
+    config_settings = ProductionConfig
+elif os.environ.get("FLASK_ENV").lower() == "testing":
+    from config import TestingConfig
+
+    config_settings = TestingConfig
+else:
+    config_settings = DevelopmentConfig
+app.config.from_object(config_settings)
+
+migrate = Migrate(app, db)
+>>>>>>> 92c7a0f... lint manage.py
 manager = Manager(app)
 
 manager.add_command("db", MigrateCommand)
